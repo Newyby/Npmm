b07b307097c89cb2931575299dd15a81b255221b
Newyby <Officialraydata@gmail.com>
2026-06-11 15:16:16 +0100

    update main.jss

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import './style.css'
+import dayjs from 'dayjs'
 
 document.querySelector('#app').innerHTML = `
   <h1>Timezone Clock</h1>
@@ -11,3 +12,15 @@ document.querySelector('#app').innerHTML = `
     Select Timezone
   </button>
 `
+
+const timeElement =
+  document.querySelector('.current-time')
+
+function updateTime() {
+  timeElement.textContent =
+    dayjs().format('HH:mm:ss')
+}
+
+updateTime()
+
+setInterval(updateTime, 1000)
